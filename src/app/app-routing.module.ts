02d04fb6eb825aadadf51425064e7f6daa80c450
Vivian-Santana <santanavivian245@gmail.com>
2022-01-19 11:54:23 -0300

    rotas criadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+/*Arrey de rotas */
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch:'full' }, /*esse path vazio (endereço do servidor) direciona para o path entrar, e o path : 'full' para todas as rotas */
+
+  {path:'entrar', component: EntrarComponent},
+  {path:'cadastrar', component: CadastrarComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
