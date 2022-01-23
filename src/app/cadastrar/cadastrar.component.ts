d7c80acc9d4b6cd5cb4514166fede5be514edfce
Vivian-Santana <santanavivian245@gmail.com>
2022-01-22 23:11:02 -0300

    consumo de AP I-

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
 
-  ngOnInit(): void {
+  confirmarSenha: string //variável que que armazena o valor do input
+  tipoUsuario: string
+
+  constructor(
+    private authService: AuthService, //injeção de dependencia: o modulo de cadastrar depende do authService para fazer o cadastro.
+    private router: Router
+    ) { }
+  
+
+  ngOnInit(){
+    window.scroll(0,0)
+  }
+
+  confirmSenha(event: any){     //método de confirmação de senha, recebendo um evento do tipo any
+    this.confirmarSenha = event.target.value  //pega o que estiver no valor do input e coloca para o confirmar senha
+  }
+  tipoUser(event:any){
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar(){
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmarSenha){
+      alert('As senhas estão incorretas.')
+    } else {
+      this.authService.cadastrar(this.user).subscribe((resp: User)=>{ 
+        this.user = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuário cadastrado com sucesso!')
+      })  // this.authService.cadastrar pega os usuarios dos inputs nas ngModels e mandar p o servidor, o subscribe vai sobrescrever esses inputs q são objetos (e não json).
+    }
+
   }
 
 }
