a7a50191d85f3e54dd922cbda3ed8c5503511505
Vivian-Santana <santanavivian245@gmail.com>
2022-01-27 23:34:59 -0300

    Implementação do put e delete em temas

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -19,8 +19,20 @@ export class TemaService {
     return this.http.get<Tema[]>('http://localhost:8080/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+  return this.http.get<Tema>(`http://localhost:8080/tema/${id}`,this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema>{//tema único (sem []) nesse caso (post) porque vai postar um tema por vez.
     return this.http.post<Tema>('http://localhost:8080/tema', tema, this.token)
   }//no typescript quando apertar em publicar o obj tema é mandado pra cá e ele é registrado nesse endpoint http://localhost:8080/tema.
-    
+
+  //tema service:
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('http://localhost:8080/tema', tema, this. token)
+  }
+
+  deleteTema(id:number){
+    return this.http.delete(`http://localhost:8080/tema/${id}`, this.token) // delete passa o parametro pela rota.
+  }
 }
