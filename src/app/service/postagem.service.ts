3d5ee6e80292487a65dbf00c66994cd676048192
Vivian-Santana <santanavivian245@gmail.com>
2022-01-30 14:41:00 -0300

    Implementação do put e delete de postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -20,7 +20,19 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://bloggenvivi.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://bloggenvivi.herokuapp.com/postagens/${id}`, this.token)
+
+  }
+
   postPostagem(postagem: Postagem) : Observable<Postagem>{
     return this.http.post<Postagem>('https://bloggenvivi.herokuapp.com/postagens', postagem, this.token)
+  }
+  putPostagem(postagem: Postagem):Observable<Postagem> {
+    return this.http.put<Postagem>('https://bloggenvivi.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id:number){
+    return this.http.delete(`https://bloggenvivi.herokuapp.com/postagens/${id}`, this.token)
   }
 }
