8daccc8d1811946c9164d7f100234ec42c189d4e
Vivian-Santana <santanavivian245@gmail.com>
2022-02-07 21:50:28 -0300

    solucionando bugs

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -19,7 +19,11 @@ export class AuthService {
   }
   cadastrar(user:User):Observable<User>{
     return this.http.post<User>('https://bloggenvivi.herokuapp.com/usuarios/cadastrar', user)
-  }  
+  } 
+  
+  atualizar(user: User): Observable<User> {
+    return this.http.put<User>('https://bloggenvivi.herokuapp.com/usuarios/atualizar', user)
+  }
 
   getByIdUser(id: number): Observable<User>{
     return this.http.get<User>(`https://bloggenvivi.herokuapp.com/$(id)`)
